a802ba5fd46d34a2516039734a7244c0f5976517
Neale Philippe <neale@adflexmarketing.com.au>
2022-07-19 14:17:20 +1000

    intial set up of Home and contact page and create react app

---
+++ my-portfolio/src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+    return(
+        
+        <section class="container">
+            <h2 class="top-title">Neale Philippe</h2>
+    <div class="row">
+        <img src="my-portfolio/src/images/me.jpeg" class="img-thumbnail" alt="me"></img>
+   <p>
+       My name is Neale Philippe and now to tell you abit about myself, i have been involved in administration operations for approx 20+ years now, i have been able to learn different skills in a wide range of areas
+            such as but not limited to; Data entry and cleaning, Working with Zoho analytics dashboards, designing various promotional material, authorising payments, computer IT, just to name a few.
+            I decided its time to learn a new skill and always have been interested in web design/development. So please feel free to look at my portfolio to see some of my developing work.
+        For further information on myself/work fell free to visit my github</p>
+ </div>
+ </section>
+    )};
+
+    export default About;
---
+++ my-portfolio/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+    return (
+      <section>
+    
+          <h1 className="heading-name">
+          Welcome to <strong className="main-name"> Neale Philippe's</strong> Portfolio</h1>
+       
+    </section>
+  );
+}
+
+export default Home;
