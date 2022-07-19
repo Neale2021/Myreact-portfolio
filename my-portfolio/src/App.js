ec2d9e2627c529a654e0219bba66bd0a37c305c7
Neale Philippe <neale@adflexmarketing.com.au>
2022-07-19 14:55:30 +1000

    updated button app.js and index.css file also created nav.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import Nav from "./components/Nav"
+import Home from "./components/Home";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import TopButton from "./components/button";
+import { BrowserRouter as Router,Route,Routes,Navigate} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Nav/>
+        <TopButton/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project" element={<Portfolio />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+    </Router>
   );
 }
+
 
 export default App;
